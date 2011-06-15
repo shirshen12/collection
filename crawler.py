8a28fe16b47d95d3c53b769abedf4ab5ed01f71a
Shirshendu Chakrabarti <shirshen12@gmail.com>
2011-06-14 23:57:47 -0700

    Added the code for multi-process crawler. Added global variables which will serve as a shared resource for all crawlers.

--- crawler.py
+++ crawler.py
@@ -2,13 +2,15 @@
 
 # This script contains the generic crawler, which will be used to crawl targetted websites.
 # By generic, we mean that by specifying:
-# 
+#
 # 1. The FULLY QUALIFIED DOMAIN NAME, henceforth FQDN, like "http://www.bestbuy.com/"
-# 2. A list of URLs (probably 10) which will act a training data set for our crawler 
+# 2. A list of URLs (probably 10) which will act a training data set for our crawler
 #    to perform structural evaluation of pages, so that it can dynamically focus and
 #    download only those pages that are of interest to the user.
 # 3. A list of CSS rules, to extract only those parts of the page which are of interest
 #    to the user, as expressed by the CSS rules.
+#
+# we can crawl any website of interest.
 #
 # Program flow:
 #
@@ -18,7 +20,7 @@
 #    of the pages at real-time based on sample URL representations and downloads
 #    the pages and stores them in a compressed format, with timestamp and URL of
 #    the page as its filename.
-# 3. Finally, apply the CSS rules, and extract content and log all that info in a 
+# 3. Finally, apply the CSS rules, and extract content and log all that info in a
 #    a log file (http://www.bestbuy.com/crawlresults/<date>/<begin_processing_timestamp>)
 #
 # At every step of the program flow, a mail to indicate the job completion step will be sent
@@ -34,31 +36,68 @@ import os
 import sys
 import time
 import random
+import urllib2
 import pdb
 
 # Import Internal modules dependencies written by in-house software developers here.
 
-import KastParsers
+# Custom parsing module with specific parsing functions.
 
+import KastParsersLib
 
+# Handy module with date and time processing functions.
+
+import KastTimeLib
+
+# Global lists of unseen and seen URLs list.
+
+BASE_DIR = '/kast/'
+CRAWL_COPIES_LIMIT = 10
+unseenUrlList = []
+seenUrlList = []
+crawlSuccessFlag = []
 
 # This function kickstarts our crawler program.
 
 def main(targetWebsite, configFile):
 
+  # List all global variables so that they can be modified and be thread safe
+
   # First read the config file into a Dictionary/Hash structure.
 
-  targetWebsiteConfigs = KastParsers.kastConfigFileParser(configFile)
+  targetWebsiteConfigs = KastParsersLib.kastConfigFileParser(configFile)
 
   # Obtain the list of URLs from the above data structure and generate DFTs for all of them.
-  
-  pass
-  
+
+  dftRepresentations = [KastParsersLib.html2dft(url) for url in targetWebsiteConfigs['SampleURLS']]
+
+  # Populate the unseenUrlList
+
+  populateUnseenUrlList(targetWebsite)
+
+  # Start crawling routine, which is multi process routine. So we will first fork 'x' copies of the crawl.
+
+  crawlerCopies = int(targetWebsiteConfigs['CrawlerCopies'])
+  child_pids = [os.fork() for i in range(0, crawlerCopies) if crawlerCopies <= CRAWL_COPIES_LIMIT]
+  child_pids = [i for i in child_pids if i == 0]
+
+  # Start a crawl process for all succesfull forks. In this
+
+  for copy in range(0, len(child_pids)):
+
+    crawlSuccessFlag.append(crawl(dftRepresentations), BASE_DIR)
+
+  # Apply the CSS rules for scrapping content, this will serve as a simple rule engine template.
+
+  # Convert the log file into RDF N Triples file
+
+  # Now log all the information to AllegroGraphDB
+
 
 if __name__ == '__main__':
-  
+
   # Check arguments being passed. We need 3 arguments:
-  # 
+  #
   # 1. Fully qualified name of the website to be crawled.
   # 2. A .json format file which has the sample list URLs
   #    which act the training dataset for the crawler.
@@ -72,6 +111,4 @@ if __name__ == '__main__':
     print 'Usage: # <BASE_DIR>/crawler.py <FQDN of website to be crawled> <Absolute filename containing list of URLs and CSS rules>\n'
     print '# /home/shirshendu/kast_collection/crawler.py http://www.bestbuy.com /home/shirshendu/bestbuy.json\n'
     print ''
-
-  
 
