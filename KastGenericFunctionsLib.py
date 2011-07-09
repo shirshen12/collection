191981ad8eb305361290e841dcb9ef60aef49338
Shirshendu Chakrabarti <shirshen12@gmail.com>
2011-07-09 16:58:06 -0700

    Added helper functions, logException() chkmkFolderStructure() and extract websitename().

--- KastGenericFunctionsLib.py
+++ KastGenericFunctionsLib.py
@@ -6,3 +6,39 @@
 # Created      :   2011-July-08
 # Modified     :   2011-July-08
 
+import os
+import sys
+import time
+
+# This function is a generic log fucntion which records exception events.
+
+def logException(event, fileName):
+
+  # Convert to string, just for safety.
+
+  event = str(event)
+
+  # File operations.
+
+  f = file(fileName, 'a') # Use append mode and absolute filename
+  f.write(event + '\n')
+  f.close()
+
+# This function check if a particular folder structure exists or not.
+# It will make it if it is non-existant, else leave it as it is.
+
+def chkmkFolderStructure(folderStruct):
+
+  if os.path.exists(folderStruct):
+    os.makedirs(folderStruct)
+
+  return folderStruct
+
+# This function extracts the website name from FQDN
+
+def extractWebSiteName(targetWebsite):
+
+  websiteName = targetWebsite.split('http://')[1]
+
+  return websiteName
+
