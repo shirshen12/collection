d0efe3760d13f462227dbdfecbd0ffc763080a32
Shirshendu Chakrabarti <shirshen12@gmail.com>
2011-07-08 19:10:57 -0700

    Added initial comments for KastGenericFunctionsLib.py module.

---
+++ KastGenericFunctionsLib.py
@@ -0,0 +1,8 @@
+# This is a generic functions module for all Kast based scripts.
+# Mainly collection specific routines will be present, but as the
+# project matures, more functions will be added.
+
+# Programmer   :   Shirshendu Chakrabarti
+# Created      :   2011-July-08
+# Modified     :   2011-July-08
+
